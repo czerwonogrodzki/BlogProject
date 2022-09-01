6dfa011566639058d3b8c05468598164125e1a29
czerwonogrodzki <czerwonop@gmail.com>
2022-09-01 12:47:38 +0200

    Created moderation and post status enums

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace BlogProject.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Offensive language")]
+        Language,
+        [Description("Illegal drugs and other substances")]
+        IllegalSubstances,
+        [Description("Racism, xenophobia, homophobia, etc.")]
+        HateSpeech,
+        [Description("Sexual content")]
+        Sexual,
+        [Description("Harrasment")]
+        Harassment,
+        [Description("Spreading false information")]
+        FalseInformation,
+        [Description("Everything that doesn't include other categories")]
+        Other
+
+    }
+}
