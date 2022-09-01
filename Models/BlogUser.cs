8ba359568534770e42999af6c9d29c6fcd65e8b7
czerwonogrodzki <czerwonop@gmail.com>
2022-09-01 12:49:19 +0200

    Added BlogUser to use instead of IdentityUser

---
+++ Models/BlogUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogProject.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "{0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [Display(Name = "Facebook link")]
+        public string FacebookUrl { get; set; }
+        [Display(Name = "Twitter link")]
+        public string TwitterUrl { get; set; }
+        [Display(Name = "Github link")]
+        //[RegularExpression("(github.com/)", ErrorMessage = "{0} is invalid.")]
+        public string GithubUrl { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        public virtual ICollection<Blog> Blogs { get; set; } = new HashSet<Blog>();
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
