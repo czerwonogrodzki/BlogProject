257bc13d95d4acf12a9e5f964cd3a12bba2c6481
czerwonogrodzki <czerwonop@gmail.com>
2022-10-07 15:17:55 +0200

    Registered services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using BlogProject.Data;
 using BlogProject.Models;
+using BlogProject.Services;
+using BlogProject.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace BlogProject
 {
@@ -14,13 +17,26 @@ namespace BlogProject
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseNpgsql(connectionString));
+            
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
             builder.Services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
                 .AddDefaultUI();
 
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddScoped<DataService>();
+
+            builder.Services.AddScoped<IBlogEmailSender, EmailService>();
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+            builder.Services.AddScoped<IImageService, ImageService>();
+            builder.Services.AddScoped<ISlugService, SlugService>();
+
             var app = builder.Build();
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -33,6 +49,8 @@ namespace BlogProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //app.Services.CreateScope().ServiceProvider.GetRequiredService<DataService>().ManageDataAsync();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
